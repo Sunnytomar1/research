ed94cd3a1a87385613f0c147c2b3ba59e8a42cba
Adarsh Raj <adarshrj62@gmail.com>
2023-10-20 22:04:50 +0530

    Minor Fix

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import "./Header.css"
-import SearchIcon from '@mui/icons-material/Search';
-
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import Logo from './assets/projectlogo.png'
 
 function Header() {
   return (
     <div>
-  <img
-          className="header__logo"
-          src=""
+      <img alt="Header" className="header__logo" src={Logo} />
+
+      <div className="header__search">
+        <input
+          className="header__searchInput"
+          type="text"
+          placeholder="Type name of research paper"
         />
-
-
-       <div className="header__search">
-        <input className="header__searchInput" type="text" placeholder="Type name of research paper" />
         <SearchIcon className="header__searchIcon" />
       </div>
-
     </div>
-  
-       
-  )
+  );
 }
 
-export default Header
+export default Header;
